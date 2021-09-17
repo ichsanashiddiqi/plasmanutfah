f812f844c88f4422ce8de36970e6f35030a084ef
unknown <iashiddiqi13@gmail.com>
2021-09-17 13:57:58 +0700

    ksad

--- application/views/tambah/tambah_tebu.php
+++ application/views/tambah/tambah_tebu.php
@@ -63,7 +63,7 @@
                                 <tr>
                                     <td><?php echo $no++ ?></td>
                                     <td><a href=""><i class="fas fa-edit" style="color: chartreuse;"></i></a>
-                                        <a href="hapus_tebu/<?= $abk->kar;"><i class="fas fa-trash-alt" style="color: red;"></i></a>
+                                        <a href="hapus_tebu/<?= $abk->kar; ?>"><i class="fas fa-trash-alt" style="color: red;"></i></a>
                                     </td>
                                     <td><?php echo $abk->kar ?></td>
                                     <td><?php echo $abk->question ?></td>
